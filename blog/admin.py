a1743658c342849e30b0865692439045017de9b8
EternalPatience <zhakupov.rm2112@gmail.com>
2021-10-30 17:23:11 +0300

    Modified templates

--- blog/admin.py
+++ blog/admin.py
@@ -1,6 +1,6 @@
+from io import DEFAULT_BUFFER_SIZE
 from django.contrib import admin
 from .models import Post
 
 admin.site.register(Post)
-
 
--- blog/models.py
+++ blog/models.py
@@ -1,11 +1,8 @@
-from datetime import time
 from django.db import models
-from django.conf import settings
-from django.db.models import deletion
 from django.utils import timezone
 
+
 class Post(models.Model):
-    author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
